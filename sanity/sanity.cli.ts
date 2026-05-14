596d6e08708566727b1e7c7ced39bd9293b7bfdb
ihack168 <ihack168@gmail.com>
2026-05-14 07:39:29 +0000

    s

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 't0di9pwy',
+    projectId: 'no0ub4vm',
     dataset: 'production'
   },
   deployment: {
