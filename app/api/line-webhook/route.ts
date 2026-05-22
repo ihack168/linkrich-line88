fd0d7cea1ff806ea3e3b23d0034b62448a0430c1
ihack168 <ihack168@gmail.com>
2026-05-22 16:47:48 +0800

    s

---
+++ app/api/line-webhook/route.ts
@@ -0,0 +1,28 @@
+export async function POST(req: Request) {
+  const body = await req.text();
+
+  try {
+    await fetch(
+      "https://script.google.com/macros/s/AKfycbwFpZDhMveHhdOYdDkh02JpWk28jUCBqikyM-Urg_6Uw2jTH7d8ZluKxinKTWh5_20N/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+
+  return new Response("ok", {
+    status: 200
+  });
+}
+
+export async function GET() {
+  return new Response("ok", {
+    status: 200
+  });
+}
